dbfd6aa227ed0b7614e4a4c9ff222a921a5110f7
zyc <zyc@hasor.net>
2017-02-19 22:37:34 +0800

    1.例子程序端口错开。

---
+++ example-spring/example-spring-consumer/src/main/java/net/example/spring/customer/CustomerClient.java
@@ -0,0 +1,44 @@
+package net.example.spring.customer;
+import net.example.domain.consumer.EchoService;
+import net.example.domain.consumer.MessageService;
+import net.example.domain.consumer.UserService;
+import net.example.domain.domain.UserDO;
+import net.hasor.rsf.RsfResult;
+import net.hasor.rsf.json.JSON;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+/**
+ *
+ * @version : 2014年9月12日
+ * @author 赵永春(zyc@hasor.net)
+ */
+public class CustomerClient {
+    public static void main(String[] args) throws Throwable {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("consumer-config.xml");
+        //
+        EchoService echoService = (EchoService) applicationContext.getBean("echoService");
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(100);
+            String res = echoService.sayHello("Hello Word");
+            System.out.println(res);
+        }
+        System.out.println("@@@@@@@@@@@@@@");
+        //
+        //
+        MessageService messageService = (MessageService) applicationContext.getBean("messageService");
+        for (int i = 0; i < 10; i++) {
+            RsfResult res = messageService.sayHello("Hello Word");
+            System.out.println(res);
+        }
+        System.out.println("@@@@@@@@@@@@@@");
+        //
+        //
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        List<UserDO> userDOS = userService.queryUser();
+        System.out.println("@@@@@@@@@@@@@@" + JSON.toString(userDOS));
+        //
+        System.out.println("server start.");
+    }
+}
